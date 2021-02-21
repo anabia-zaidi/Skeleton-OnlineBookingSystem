11a1b18df65217cc103f758b4346710d1d2cc109
Safwaan <safwaanhusen@outlook.com>
2021-02-21 18:28:03 +0000

    added testing and class for scheduling management

---
+++ Testing4/tstSchedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstSchedule
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsSchedule ABooking = new clsSchedule();
+            Assert.IsNotNull(ABooking);
+        }
+    }
+}

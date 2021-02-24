e33ff8e239d0c04890eedd57ca835cb777b070ef
Syeda Zaidi <P2453245@my365.dmu.ac.uk>
2021-02-24 00:46:34 +0000

    Test for each attribute in my table schema with an associated property generated from my testing procedure added.

--- Testing1/tstServices.cs
+++ Testing1/tstServices.cs
@@ -14,5 +14,51 @@ namespace Testing1
             clsServices AService = new clsServices();
             Assert.IsNotNull(AService);
         }
+
+        [TestMethod]
+        public void DiscountPropertyOK()
+        {
+            clsServices AService = new clsServices();
+            Boolean TestData = true;
+            AService.Discount = TestData;
+            Assert.AreEqual(AService.Discount, TestData);
+
+        }
+
+        [TestMethod]
+        public void ServiceNamePropertyOK()
+        {
+            clsServices AService = new clsServices();
+            string TestData = "Cut and Blowdry";
+            AService.ServiceName = TestData;
+            Assert.AreEqual(AService.ServiceName, TestData);
+        }
+        [TestMethod]
+        public void StaffPropertyOK()
+        {
+            clsServices AService = new clsServices();
+            string TestData = "Amy";
+            AService.Staff = TestData;
+            Assert.AreEqual(AService.Staff, TestData);
+
+        }
+
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            clsServices AService = new clsServices();
+            Int32 TestData = 1;
+            AService.Price = TestData;
+            Assert.AreEqual(AService.Price, TestData);
+        }
+
+        [TestMethod]
+        public void DurationPropertyOK()
+        {
+            clsServices AService = new clsServices();
+            Int32 TestData = 1;
+            AService.Duration = TestData;
+            Assert.AreEqual(AService.Duration, TestData);
+        }
     }
 }

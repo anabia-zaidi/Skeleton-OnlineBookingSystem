272a3693e1ffebd8499699c633bc0cba2bb40b03
Syeda Zaidi <P2453245@my365.dmu.ac.uk>
2021-02-24 06:39:39 +0000

    Added a basic presentation layer

--- AdminSystem/1ServicesView.aspx.cs
+++ AdminSystem/1ServicesView.aspx.cs
@@ -6,19 +6,13 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using ClassLibrary;
 
-public partial class _1_DataEntry : System.Web.UI.Page
+public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
-    }
-
-    protected void btnOK_Click(object sender, EventArgs e)
-    {
         clsServices AService = new clsServices();
-        AService.ServiceName = txtServiceName.Text;
-        Session["AService"] = AService;
-        Response.Redirect("1ServicesView.aspx");
-
+        AService = (clsServices) Session["AService"];
+        Response.Write(AService.ServiceName);
     }
+
 }

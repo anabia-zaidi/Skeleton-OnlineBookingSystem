8bd229b440a148cb0887c1403f323a567a36b3b4
P2501016 <P2501016@users.noreply.github.com>
2021-03-08 13:06:05 +0000

    The Marketing from test5 now has been changed to EmployeeId

---
+++ Testing5/UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing5
+{
+    [TestClass]
+    public class profitOrLoss
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsProfitOrLoss AProfitOrLoss = new clsProfitOrLoss();
+            //test to see if it exists
+            Assert.IsNotNull(AProfitOrLoss);
+        }
+    }
+}
